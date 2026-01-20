df313d424fc8fca0347cae8dcc3b085098c64edd
AdityaChafale05 <adityachafale2006@gmail.com>
2026-01-20 20:40:55 +0530

    small changes

--- .ipynb_checkpoints/demo_plot-checkpoint.py
+++ .ipynb_checkpoints/demo_plot-checkpoint.py
@@ -1,16 +1,55 @@
-import matplotlib.pyplot as plt
+#with plt.xkcd():   (use  indentation)         #use this for comic style plot 
+
+""" To check for available styles in matplotlib 
+    plt.style.available
+    
+    for implementing a style:
+    plt.style.use('style_name')
+"""
 
 
-days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-calories = [2100, 2400, 1900, 2200, 2500]
-
-fig, ax = plt.subplots(figsize=(6,4))
-
-ax.bar (days, calories, color ='skyblue')
-#ax.fill (days, calories , color ='skyblue')
-ax.set_ylabel("calories burned this week ")
-ax.set_xlabel("calories")
+# Import matplotlib's pyplot module for data visualization
+import matplotlib.pyplot as plt 
 
 
-plt.plot(days,calories,'o',color='red')
+# List of months used for x-axis (Jan to Dec)
+months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+#prices of Ram in 2024-25
+prices_2025 = [3200,3300,3450,3600,3800,3950,4100,4050,3900,3700,3550,3400]
+prices_2024 = [1100,1120,1180,1240,1290,1250,1250,350,420,560,700,850]
+
+plt.plot(
+    months,                    #data for months
+    prices_2025,               #data for prices in 2025
+    color="red",               #line color (red)
+    linestyle=':',             #line style (dotted line)
+    marker='o',                #marker style (circle)
+    label='2025'               #label for the legend
+    )
+
+plt.plot(months,           #data for months
+        prices_2024,       #data for prices in 2024        
+        color="blue",      #line color (blue)
+        linestyle='--',    #line style (dashed line)
+        marker='^',        #marker style (triangle)
+        label='2024'       #label for the legend
+        )
+# Label the x-axis
+plt.xlabel("Months")
+
+# Label the y-axis
+plt.ylabel("Cost (₹)")
+
+plt.title("DDR4 8GB RAM Prices (2024–2025)")
+
+# Enable grid for better readability
+plt.grid(True)
+
+# Display legend to differentiate between years
+plt.legend()
+
+# Render the plot on screen
 plt.show()
+
+    
